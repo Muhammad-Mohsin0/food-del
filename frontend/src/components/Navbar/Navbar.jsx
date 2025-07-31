4126ce4cc2138a4dcdc260380eb76ca70162f1ef
Muhammad-Mohsin0 <mohsin.signin00@gmail.com>
2025-07-31 04:21:41 -0700

    responsive

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,31 +1,48 @@
-import React, { useState } from 'react'
-import './Navbar.css'
-import {assets} from "../../assets/assets.js"
+import React, { useState } from "react";
+import "./Navbar.css";
+import { assets } from "../../assets/assets.js";
 //efefefefefdfd
 const Navbar = () => {
-
-    const [menu, setMenu]= useState("home")
+  const [menu, setMenu] = useState("home");
   return (
-    <div className='navbar'>
+    <div className="navbar">
       <img src={assets.logo} alt="" className="logo" />
       <ul className="navbar-menu">
-        < li onClick={()=>setMenu("home")} className={menu==="home" ?"active":""}>home</li>
-        < li onClick={()=>setMenu("menu")} className={menu==="menu" ?"active":""}>menu</li>
-        < li onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app" ?"active":""}>mobile-app</li>
-        < li onClick={()=>setMenu("contact us")} className={menu==="contact us" ?"active":""}>contact us</li>
+        <li
+          onClick={() => setMenu("home")}
+          className={menu === "home" ? "active" : ""}
+        >
+          home
+        </li>
+        <li
+          onClick={() => setMenu("menu")}
+          className={menu === "menu" ? "active" : ""}
+        >
+          menu
+        </li>
+        <li
+          onClick={() => setMenu("mobile-app")}
+          className={menu === "mobile-app" ? "active" : ""}
+        >
+          mobile-app
+        </li>
+        <li
+          onClick={() => setMenu("contact us")}
+          className={menu === "contact us" ? "active" : ""}
+        >
+          contact us
+        </li>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
-            <img src={assets.basket_icon} alt="" />
-            <div className="dot"></div>
-            
-
+          <img src={assets.basket_icon} alt="" />
+          <div className="dot"></div>
         </div>
         <button>sign in</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;

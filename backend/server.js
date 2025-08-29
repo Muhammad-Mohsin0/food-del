e25e42b2386c364f8115d4c61d5bfdc00e7dbc4e
Muhammad-Mohsin0 <mohsin.signin00@gmail.com>
2025-08-28 23:20:53 -0700

    mongoDb connection

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,27 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+dotenv.config({ path: "./.env" }); // Load .env
 
 // app config
-const app =express()
-const port =5000
+const app = express();
+const port = process.env.PORT || 5000;
 
 // middleware
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.get("/",(req,res)=>{
-    res.send("API Working")
-})
+// db connection
+connectDB();
 
-app.listen(port, ()=>{
-    console.log(`Server is started on http://localhost:${port}`)
-})
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
+
+app.listen(port, () => {
+  console.log(`Server is started on http://localhost:${port}`);
+});
+
+// mongodb+srv://mohsinsignin:2gKlR3X8PdfTZwKG@cluster0.lqsg9co.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0

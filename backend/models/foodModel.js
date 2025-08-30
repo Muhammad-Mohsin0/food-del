d93a2a6846f8368906bc93b2737c7df6b5c0501b
Muhammad-Mohsin0 <mohsin.signin00@gmail.com>
2025-08-29 23:41:00 -0700

    food model

---
+++ backend/models/foodModel.js
@@ -0,0 +1,24 @@
+import mongoose, { Model } from "mongoose";
+
+const foodSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  discription: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+});

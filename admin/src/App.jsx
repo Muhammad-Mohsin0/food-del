12f052bd9b5c140bdab558825f20a69f3d9c06c9
Muhammad-Mohsin0 <mohsin.signin00@gmail.com>
2025-11-26 00:52:25 -0800

    adding food

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,13 +1,17 @@
-import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Sidebar from "./components/Sidebar/Sidebar";
 import { Routes, Route } from "react-router-dom";
 import Add from "./pages/Add/Add";
 import Order from "./pages/Orders/Order";
 import List from "./pages/List/List";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
+
+
 const App = () => {
   return (
     <div>
+      <ToastContainer/>
       <Navbar />
       <hr />
       <div className="app-content">

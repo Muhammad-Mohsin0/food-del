f372405c51b3b9b2a04a22df34d18224b7fd115f
Muhammad-Mohsin0 <mohsin.signin00@gmail.com>
2025-11-26 08:09:51 -0800

    login/register

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
+// import 'dotenv/config'
 
 dotenv.config({ path: "./.env" }); // Load .env
 
@@ -20,6 +22,7 @@ connectDB();
 // api endpoints
 app.use("/api/food", foodRouter);
 app.use("/images", express.static("uploads"));
+app.use("/api/user", userRouter)
 
 app.get("/", (req, res) => {
   res.send("API Working");
